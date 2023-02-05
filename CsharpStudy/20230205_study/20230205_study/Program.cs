00f8edc64e911ecc62686074466f8f8054387acd
zesilver <cotton_vu@naver.com>
2023-02-05 17:48:29 +0900

    20230205_study

---
+++ CsharpStudy/20230205_study/20230205_study/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230205_study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. 이름 입력받기
+            System.Console.Write("이름을 입력하세요 : ");
+            string myName = System.Console.ReadLine();
+            System.Console.WriteLine(myName);
+
+            //2. 생년월일 입력받기
+            System.Console.Write("생년을 입력하세요 : ");
+            var myBirth = System.Console.ReadLine();
+            int birth = Convert.ToInt32(myBirth);
+            System.Console.WriteLine(birth);
+
+            //3. 자기소개 입력받기(1줄)
+            System.Console.Write("자기소개를 써주세요 : ");
+            string myIntro = System.Console.ReadLine();
+            System.Console.WriteLine(myIntro);
+
+            //4. writeline으로 출력하기(생년월일 -> 나이로)
+            string nowAge = (2023 - birth).ToString();
+            System.Console.WriteLine(nowAge);
+
+            //5. $ 넣어서 표현하기
+            System.Console.WriteLine($"저의 이름은 {myName}이고, 태어난 년도는 {birth}년 입니다. 저를 소개하자면 {myIntro} 마지막으로 나이는 {nowAge}살 입니다.");
+
+            //6. @를 넣어서 사각형 그리기
+            System.Console.WriteLine(@"
+@@@@@@@@@@@@@@@@@
+@               @
+@               @
+@               @
+@               @
+@@@@@@@@@@@@@@@@@
+
+@--->----...장미교실
+");
+
+            //7. 1 2 3 string 배열에 넣기
+            string[] array = new string[3] { myName, myBirth, myIntro };
+
+            //8. string 배열값은 4번과 같은 방식으로 출력하기
+            System.Console.WriteLine(array[0]);
+            System.Console.WriteLine(array[1]);
+            System.Console.WriteLine(array[2]);
+
+            //9. string 배열값은 5번과 같은 방식으로 출력하기
+            System.Console.WriteLine($"저의 이름은 {array[0]}이고, 탄생년도는 {array[1]}년이고, 저를 소개하면 {array[2]}");
+        }
+    }
+}
--- CsharpStudy/CsharpStudy/Program.cs
+++ CsharpStudy/CsharpStudy/Program.cs
@@ -9,7 +9,7 @@ namespace CsharpStudy
     class Program
     {
         static void Main(string[] args)
-        {
+        {  /*
             System.Console.WriteLine("Hello World");
             System.Console.WriteLine("내 이름은 서영은 입니다");
             System.Console.WriteLine("나는 28살 입니다");
@@ -41,7 +41,7 @@ namespace CsharpStudy
 
             //데이터 형 - string : 문장 형식
 
-            /* String myAge;
+           String myAge;
              String myHome;
              String myMbti;
              String myPet;
@@ -285,7 +285,7 @@ namespace CsharpStudy
             food[2] = "라면";
             food[3] = "라볶이";
 
-                */
+               
 
             string[] _lang = new string[3];
             _lang[0] = "c";
@@ -323,13 +323,610 @@ namespace CsharpStudy
 
 
 
+            
+
+            //Null : 값이 비어있다
+
+            //산술이항연술자
+            int number1 = 10;
+            int number2 = 12;
+
+            int plusResult = number1 + number2;
+            int minusResult = number1 + number2;
+            int multResult = number1 * number2;
+            int quioResult = number1 / number2; //몫
+            int reminResult = number1 % number2; //나머지
 
 
+            System.Console.WriteLine($"Plus result : {plusResult}");
+            System.Console.WriteLine($"minusResult result : {minusResult}");
+            System.Console.WriteLine($"multResult result : {multResult}");
+            System.Console.WriteLine($"quioResult result : {quioResult}");
+            System.Console.WriteLine($"reminResult result : {reminResult}");
+
+             
+
+            //할당연산자 : 있는 값에 바로 연산하기
+            int number11 = 123;
+            int number21 = 532;
 
 
+            number21 += number11; //=> number21 값에 number11값을 더해 나온 값을 number21에 넣겠다
+            System.Console.WriteLine($"PP result : {number21}");
+
+            number21 -= number11; 
+            System.Console.WriteLine($"PP result : {number21}");
+
+            number21 *= number11;
+            System.Console.WriteLine($"PP result : {number21}");
+
+            number21 /= number11;
+            System.Console.WriteLine($"PP result : {number21}");
+
+            number21 %= number11;
+            System.Console.WriteLine($"PP result : {number21}");
+
+           
+            int value = 123;
+
+            System.Console.WriteLine($"result : {++value}");
+            System.Console.WriteLine($"result : {value}");
+            System.Console.WriteLine($"result : {value++}");
+            System.Console.WriteLine($"result : {value}"); 
+          
 
 
+            int a = 1;
+            int b = 2;
 
+            int plus = a + b;
+            int minus = a - b;
+            int mult = a * b;
+            int quio = a / b;
+            int remin =  a % b;
+
+            System.Console.WriteLine($"plus : {plus}");
+            System.Console.WriteLine($"plus : {minus}");
+            System.Console.WriteLine($"plus : {mult}");
+            System.Console.WriteLine($"plus : {quio}");
+            System.Console.WriteLine($"plus : {remin}");
+
+
+            int a11 = 2;
+            int a12 = 2;
+            int a13 = 2;
+            int a14 = 2;
+            int a15 = 2;
+
+            int b22 = 5;
+
+            a11 += b22;
+            a12 -= b22;
+            a13 *= b22;
+            a14 /= b22;
+            a15 %= b22;
+
+            System.Console.WriteLine($"plus : {a11}");
+            System.Console.WriteLine($"plus : {a12}");
+            System.Console.WriteLine($"plus : {a13}");
+            System.Console.WriteLine($"plus : {a14}");
+            System.Console.WriteLine($"plus : {a15}");
+
+            int c = 10;
+
+            System.Console.WriteLine($"값 {++c}");
+            System.Console.WriteLine($"값 {c}");
+            System.Console.WriteLine($"값 {c++}");
+            System.Console.WriteLine($"값 {c++}");
+
+           
+
+
+            // const : 한 번 저장하면 다시 값을 못바꿈
+           
+            const int theNumber = 10;
+
+            // bool : ture 또는 false 만 넣을 수 있음 보통 변수 앞에 is가 붙음
+            // 변수명 앞에 !(느낌표)가 붙으면 값이 반대로 된다.
+            bool isName = true;
+            bool isYouName = !isName;
+            
+            bool isTrue = true;
+            bool isFalse =false;
+
+
+            // 비교 연산자
+            // == : 같다
+            // != : 다르다
+            // < : 작다
+            // > : 크다
+            // <= : 작거나 같다
+            // >= : 크거나 같다
+
+            bool isSame = (3 == 3);
+            System.Console.WriteLine(isSame); //true
+
+            bool isNo = (4 != 3);
+            System.Console.WriteLine(isNo); //true
+
+            bool isS = (5 < 10);
+            System.Console.WriteLine(isS); //true
+
+            bool isB = (1 > 3);
+            System.Console.WriteLine(isB); //false
+
+            bool isSs = (3 <= 3);
+            System.Console.WriteLine(isSs); // true
+
+            bool isBb = (3 >= 8);
+            System.Console.WriteLine(isBb); //false
+
+            bool isDif = ("hello" != "hello");
+            bool isdiff = (true == false);
+            
+
+
+            //논리연산자!!
+
+            bool isTrue = true;
+            bool isFalse = false;
+
+            bool isAnd = (isTrue && isFalse); //둘 중 하나라도 거짓이면 거짓(즉, 둘 다 참이거나 둘 다 거짓이어야 함)
+            bool isOr = (isTrue || isFalse); //둘 중 하나만 참이어도 참
+
+            System.Console.WriteLine(true || true); //true
+            System.Console.WriteLine(true && false); //false
+            System.Console.WriteLine(false && false); //false
+            System.Console.WriteLine(false && true); //false
+
+
+            System.Console.WriteLine(true || true); //true
+            System.Console.WriteLine(true || false); //true
+            System.Console.WriteLine(false || false); //true
+            System.Console.WriteLine(false || true); //false
+
+            
+
+            bool isSame = (3 == 3);
+            System.Console.WriteLine(isSame); //true
+
+            bool isNo = (4 != 3);
+            System.Console.WriteLine(isNo); //true
+
+            bool isS = (5 < 10);
+            System.Console.WriteLine(isS); //true
+
+            bool isB = (1 > 3);
+            System.Console.WriteLine(isB); //false
+
+            bool isSs = (3 <= 3);
+            System.Console.WriteLine(isSs); // true
+
+            bool isBb = (3 >= 8);
+            System.Console.WriteLine(isBb); //false
+
+            bool isDif = ("hello" != "hello");
+            bool isdiff = (true == false);
+
+            bool isTrue = true;
+            bool isFalse = false;
+
+            bool isAnd = (isTrue && isFalse); 
+            bool isOr = (isTrue || isFalse);
+
+            System.Console.WriteLine(true || true); //true
+            System.Console.WriteLine(true && false); //false
+            System.Console.WriteLine(false && false); //false
+            System.Console.WriteLine(false && true); //false
+
+            System.Console.WriteLine(true || true); //true
+            System.Console.WriteLine(true || false); //true
+            System.Console.WriteLine(false || false); //true
+            System.Console.WriteLine(false || true); //false
+          
+
+            //if문 
+            //if문 안에서 쓰여진 건 외부에서 쓸 수 없다
+            //if는 한번 쓰고 else if는 계속 쓸 수 있음 else는 단 한번 else if 전부가 아닌 경우 else 실행
+
+            if(true)
+            {
+                System.Console.WriteLine("if문 실행");
+            }
+ 
+
+            if (false)
+            {
+                System.Console.WriteLine("if문 실행");
+            }
+            else if (number < 2)
+            {
+                System.Console.WriteLine("elseif01 실행");
+            }
+            else if (number < 4)
+            {
+                System.Console.WriteLine("elseif02 실행"); 
+            }
+            else
+            {
+                System.Console.WriteLine("else문 실행");
+            }
+ 
+
+            int number = 2 + 3;
+
+            if (number < 0)
+            {
+                System.Console.WriteLine("0보다 작습니다");
+            }
+            else if (number < 2)
+            {
+                System.Console.WriteLine("0보다 크고 2보다 작습니다");
+            }
+            else if (number < 4)
+            {
+                System.Console.WriteLine("2보다 크고 4보다 작습니다"); 
+            }
+            else
+            {
+                System.Console.WriteLine("4보다 큽니다");
+            }
+          
+
+            System.Console.Write("첫번째 숫자를 입력하세요 : ");
+            var a = System.Console.ReadLine();
+            int aNumber = Convert.ToInt32(a);
+            System.Console.Write("두번째 숫자를 입력하세요 : ");
+            var b = System.Console.ReadLine();
+            int bNumber = Convert.ToInt32(b);
+
+            int abNumber = aNumber + bNumber;
+
+            if (abNumber < 10)
+            {
+                System.Console.WriteLine("10보다 작습니다");
+            }
+            else if (abNumber < 20)
+            {
+                System.Console.WriteLine("10보다 크고 20보다 작습니다");
+            }
+            else if (abNumber < 40)
+            {
+                System.Console.WriteLine("20보다 크고 40보다 작습니다");
+            }
+            else
+            {
+                System.Console.WriteLine("40보다 큽니다");
+            }  
+           
+
+            //A가 짝수이고 B가 0보다 작흠
+            //A가 짝수이고 B가 3보다 작음
+            //A가 홀수이고 5보다 작음
+            //else
+            System.Console.Write("첫번째 숫자를 입력하세요 : ");
+            var a = System.Console.ReadLine();
+            int aNumber = Convert.ToInt32(a);
+            System.Console.Write("두번째 숫자를 입력하세요 : ");
+            var b = System.Console.ReadLine();
+            int bNumber = Convert.ToInt32(b);
+
+            if (aNumber % 2 == 0 && bNumber < 0)
+            {
+                System.Console.WriteLine("A는 짝수이고 B가 0보다 작습니다");
+            }
+            else if (aNumber % 2 == 0 && bNumber < 3)
+            {
+                System.Console.WriteLine("A는 짝수이고 B가 3보다 작습니다");
+            }
+            else if (aNumber % 2 == 1 && bNumber < 5)
+            {
+                System.Console.WriteLine("A는 홀수이고 B가 5보다 작습니다");
+            }
+            else 
+            {
+                System.Console.WriteLine("XXXXX");
+            }
+
+
+            //while문 : 반복문, 조건식이 false가 될 때까지 실행함, 조건을 비교하고 실행
+            int numberLimit = 10;
+            while (numberLimit > 0)
+            {
+                System.Console.WriteLine("Hi");
+
+                numberLimit--;
+            } 
+
+
+            //dowhile : 일단 실행하고 비교함, 최소 한 번은 실행해야 할 때 사용함.
+            do
+            {
+                System.Console.WriteLine("Hi");
+            } while (numberLimit > 5);
+
+            //숫자를 입력받고 그 숫자만큼 문장을 띄우기
+
+            System.Console.Write("숫자를 입력해주세요 : ");
+            var a = System.Console.ReadLine();
+            int aNum = Convert.ToInt32(a);
+
+            while(aNum > 0)
+            {
+                System.Console.WriteLine("입력 받은 숫자");
+
+                aNum--;
+            }
+
+            //for문(초기식; 조건식; 증감식) : while과 if를 합친거~!
+
+            for (int number = 0; number < 5; number++) 
+            {
+                System.Console.WriteLine("Hi");
+            }
+
+            for (int star = 0; star < 5; star++)
+            {
+                for (int starr = 0; starr < star + 1; starr++)
+                {
+                    System.Console.Write("★");
+                }
+
+                System.Console.WriteLine("");
+            }
+            System.Console.WriteLine("");
+
+            for (int star2 = 0; star2 < 5; star2++)
+            {
+                for (int starr2 = 5; starr2 > star2; starr2--)
+                {
+                    System.Console.Write("★");
+                }
+
+                System.Console.WriteLine("");
+            }
+
+            System.Console.WriteLine("");
+
+            //switch(비교대상)
+            int myNumber = 3;
+            switch (myNumber)
+            {
+                case 0:
+                    System.Console.WriteLine("0");
+                    break;
+                case 1:
+                    System.Console.WriteLine("1");
+                    break;
+                case 2:
+                    System.Console.WriteLine("2");
+                    break;
+                case 3:
+                    System.Console.WriteLine("3");
+                    break;
+                default:
+                    break;
+            }
+
+
+            //게임 끝낼 때 씀!! continue / break
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("0");
+                    continue; //위로 올라감
+                }
+                else if (i == 1)
+                {
+                    Console.WriteLine("1");
+                    Console.WriteLine("11");
+                    continue;
+                    Console.WriteLine("111");
+                }
+                else if (i == 2)
+                {
+                    Console.WriteLine("2");
+                    break; //탈출
+                }
+                else if (i == 3)
+                {
+                    Console.WriteLine("3");
+ 
+                }
+            }
+            Console.WriteLine("끝");
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("★");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+
+            for (int a = 0; a < 5; a++)
+            {
+                for (int b = 5; b > a; b--)
+                {
+                    Console.Write("★");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("");
+
+            for (int q = 0; q < 5; q++)
+            {
+                for (int w = 4; w > q; w--)
+                {
+                    Console.Write("　");
+                }
+                for (int e = 0; e < q+1; e++)
+                {
+                    Console.Write("★");
+                }
+                Console.WriteLine("");
+            }
+            
+            for (int z = 0; z < 5; z++)
+            {
+                for (int x = 5; x > z; x--)
+                {
+                    Console.Write("★");
+                }
+                for (int c = 1; c < z + 1; c++)
+                {
+                    Console.Write("　");
+                }
+                Console.WriteLine("");
+            }
+
+            //메소드
+            Console.WriteLine("1");
+            ShowUpNewSentence(5);
+           int newInt = ShowUpNewSentence(5);
+           int newa = ShowUpNewPlus(5);
+           int newb = ShowUpNewMinus(5);
+           int newd = ShowUpNewDiv(5);
+           int newc = ShowUpNewmo(5);
+
+            System.Console.WriteLine(newInt);
+            System.Console.WriteLine(newa);
+            System.Console.WriteLine(newb);
+            System.Console.WriteLine(newd);
+            System.Console.WriteLine(newc);
+
+            System.Console.Write("숫자를 입력하세요 : ");
+            var a = System.Console.ReadLine();
+            int c = Convert.ToInt32(a);
+            int hol = holjak(c);
+
+            string s1 = System.Console.ReadLine();
+            int s1Number = Convert.ToInt32(s1);
+
+            string s2 = System.Console.ReadLine();
+            int s2Number = Convert.ToInt32(s2);
+
+            string s3 = System.Console.ReadLine();
+
+            
+            
+
+            if (s3 == "+")
+            {
+                int newInt1 = plusTwoParam(s1Number, s2Number);
+                System.Console.WriteLine(newInt1);
+            }
+            else if (s3 == "-")
+            {
+                int newInt2 = minusTwoParam(s1Number, s2Number);
+                System.Console.WriteLine(newInt2);
+            }
+            else if (s3 == "*")
+            {
+                int newInt3 = gobTwoParam(s1Number, s2Number);
+                System.Console.WriteLine(newInt3);
+            }
+            else
+            {
+                System.Console.WriteLine("다시 입력해주세요");
+            }*/
+
+            System.Console.Write("숫자를 입력하세요 : ");
+            var a = System.Console.ReadLine();
+            int b = Convert.ToInt32(a);
+
+            Sentence(b);
+            
         }
+
+   
+        private static int Sentence(int Number)
+        {
+            while (Number > 0)
+            {
+                System.Console.WriteLine("안녕하세요");
+
+                Number--;
+            }
+
+            return Number;
+        }
+        private static int plusTwoParam(int Number1, int Number2)
+        {
+            int resultNumber = Number1 + Number2;
+            System.Console.WriteLine(resultNumber);
+
+            return resultNumber;
+        }
+        private static int minusTwoParam(int Number1, int Number2)
+        {
+            int resultNumber = Number1 - Number2;
+            System.Console.WriteLine(resultNumber);
+
+            return resultNumber;
+        }
+        private static int gobTwoParam(int Number1, int Number2)
+        {
+            int resultNumber = Number1 * Number2;
+            System.Console.WriteLine(resultNumber);
+
+            return resultNumber;
+        }
+        private static int ShowUpNewSentence(int number)
+        {
+            int newNumber2 = number * 5;
+            System.Console.WriteLine(newNumber2);
+
+            return newNumber2;
+        }
+        private static int ShowUpNewPlus(int number)
+        {
+            int newNumber2 = number + 5;
+            System.Console.WriteLine(newNumber2);
+
+            return newNumber2;
+        }
+        private static int ShowUpNewMinus(int number)
+        {
+            int newNumber2 = number - 5;
+            System.Console.WriteLine(newNumber2);
+
+            return newNumber2;
+        }
+        private static int ShowUpNewDiv(int number)
+        {
+            int newNumber2 = number / 5;
+            System.Console.WriteLine(newNumber2);
+
+            return newNumber2;
+        }
+
+        private static int ShowUpNewmo(int number)
+        {
+            int newNumber2 = number % 5;
+            System.Console.WriteLine(newNumber2);
+
+            return newNumber2;
+        }
+
+        private static int holjak(int number)
+        {
+            
+            if (number % 2 == 0)
+            {
+                System.Console.WriteLine("짝수 입니다.");
+            }
+            else 
+            {
+                System.Console.WriteLine("홀수 입니다.");
+            };
+
+            return number;
+        }
+
     }
 }
