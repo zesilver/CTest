bcb98a49c7687d08b3c51ef163f4fc5a396c424f
zesilver <yusung910@nate.com>
2023-02-19 17:39:36 +0900

    20230219_수업

--- Csharp_과제/3주차/20230213_과제/20230213_과제/ConsoleApp1/Program.cs
+++ Csharp_과제/3주차/20230213_과제/20230213_과제/ConsoleApp1/Program.cs
@@ -6,7 +6,48 @@ namespace consoleApp2
     {
         static void Main(string[] args)
         {
+            int myMoney = 100;
 
+            Bank Abank = new Bank();
+            Bank Bbank = new Bank();
+
+            Abank.choice();
         }
+    }
+
+    class Bank
+    {
+        public int Money;
+
+        public void input(int inputMoney)
+        {
+            int inmoney = Money + inputMoney;
+            System.Console.WriteLine($"현재 계좌 잔액은 : {inmoney}");
+            return inputMoney;
+        }
+
+        public void output(int outputMoney)
+        {
+            int outmoney = Money + outputMoney;
+            System.Console.WriteLine($"현재 계좌 잔액은 : {Money - outputMoney}");
+            return outputMoney;
+        }
+
+        public void move()
+        {
+            
+        }
+
+        public void choice()
+        {
+            System.Console.WriteLine("1. 입금");
+            System.Console.WriteLine("2. 출금");
+            System.Console.WriteLine("3. 이체");
+
+           
+        }
+
+       
+        
     }
 }
